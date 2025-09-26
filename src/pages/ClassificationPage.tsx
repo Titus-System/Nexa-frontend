7707ab7413ea3a84bb7ea9ff52a09ca4e5c085c3
juliasoares17 <juliapereira@gmail.com>
2025-09-26 16:06:50 -0300

    style(pages): Adicionando mais detalhes de estilo e ajustes de espaçamento à página de Análise de Resultados

--- src/pages/ClassificationPage.tsx
+++ src/pages/ClassificationPage.tsx
@@ -29,29 +29,32 @@ const SubmitPage: React.FC = () => {
   };
 
   return (
-    <div className="p-6">
+    <div className="p-6 flex flex-col justify-center items-center">
       <div className="mb-24">
         <h1 className="text-4xl font-bold mb-4 text-[#010A26]">Análise de Informações</h1>
         <p>Faça upload do PDF ou insira o Part Number manualmente para classificação fiscal automática.</p>
       </div>
-        <h2 className="font-semibold text-[#010A26] text-2xl mb-3 text-left">Upload de Documento</h2>
-        <div className="bg-white h-[20rem] w-[100%] flex justify-center items-center flex-col">
-          <FontAwesomeIcon icon={faArrowUpFromBracket} className="text-[#9799A6] text-4xl"/>
-          <h3 className="text-[#010A26] font-medium">Arraste o PDF aqui</h3>
-          <p className="text-[#9799A6]">Ou clique para selecionar arquivo</p>
+      <div className="w-[90%]">
+        <h2 className="font-semibold text-[#010A26] text-2xl mb-4 text-left">Upload de Documento</h2>
+        <div className="bg-white h-[20rem] w-[100%] flex justify-center items-center flex-col gap-6 border-2 border-dashed border-[#082640] rounded-xl mb-16">
+          <FontAwesomeIcon icon={faArrowUpFromBracket} className="text-[#9799A6] text-[4rem]"/>
+          <div>
+            <h3 className="text-[#010A26] font-medium">Arraste o PDF aqui</h3>
+            <p className="text-[#9799A6]">Ou clique para selecionar arquivo</p>
+          </div>
           <button className="font-bold bg-[#0F3B57] py-2 px-3 text-white rounded-lg">Selecionar PDF</button>
         </div>
-        <h2 className="font-semibold text-[#010A26] text-2xl text-left">Entrada Manual</h2>
-        <form onSubmit={handleSubmit} className="space-y-4">
-          <div className="text-left">
+        <h2 className="font-semibold text-[#010A26] text-2xl text-left mb-7">Entrada Manual</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="text-left space-y-4 mb-14">
             <label className="font-medium" htmlFor="partnumber">Part Number *</label>
-            <input className="border p-2 w-full bg-white border-[#082640] rounded-md mt-[0.8rem]" name="partnumber" placeholder="Ex: STM32F407VGT6, LM358N, etc." value={form.partnumber} onChange={handleChange} required />
+            <input className="border p-2 w-full bg-white border-[#082640] rounded-md mt-[0.6rem]" name="partnumber" placeholder="Ex: STM32F407VGT6, LM358N, etc." value={form.partnumber} onChange={handleChange} required />
             <label className="font-medium" htmlFor="partnumber">Descrição (Opcional)</label>
-            <input className="border p-2 w-full bg-white border-[#082640] rounded-md mt-[1rem]" name="description" placeholder="Descrição adicional do componente" value={form.description} onChange={handleChange} />
+            <input className="border p-2 w-full bg-white border-[#082640] rounded-md mt-[0.6rem]" name="description" placeholder="Descrição adicional do componente" value={form.description} onChange={handleChange} />
             <label className="font-medium" htmlFor="partnumber">Fabricante (Opcional)</label>
-            <input className="border p-2 w-full bg-white border-[#082640] rounded-md mt-[1rem]" name="manufacturer" placeholder="Nome do fabricante" value={form.manufacturer} onChange={handleChange} />
+            <input className="border p-2 w-full bg-white border-[#082640] rounded-md mt-[0.6rem]" name="manufacturer" placeholder="Nome do fabricante" value={form.manufacturer} onChange={handleChange} />
             <label className="font-medium" htmlFor="partnumber">Fornecedor (Opcional)</label>
-            <input className="border p-2 w-full bg-white border-[#082640] rounded-md mt-[1rem]" name="supplier" placeholder="Nome do fornecedor" value={form.supplier} onChange={handleChange} />
+            <input className="border p-2 w-full bg-white border-[#082640] rounded-md mt-[0.6rem]" name="supplier" placeholder="Nome do fornecedor" value={form.supplier} onChange={handleChange} />
           </div>
           <div className="flex justify-end gap-4">
             <button type="reset" className="rounded-lg font-medium border-1 py-2 px-3 border-[#0F3B57]">Limpar dados</button>
@@ -61,6 +64,7 @@ const SubmitPage: React.FC = () => {
             </button>
           </div>
         </form>
+      </div>
       </div>
     );
   };
