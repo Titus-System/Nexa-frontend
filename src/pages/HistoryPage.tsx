1a5c3cce8deb52508d957ed31c2b940d2500388c
juliasoares17 <juliapereira@gmail.com>
2025-10-20 10:33:32 -0300

    style(pages): Dando continuidade à estilização da página de Histórico

--- src/pages/HistoryPage.tsx
+++ src/pages/HistoryPage.tsx
@@ -36,9 +36,11 @@ const HistoryPage: React.FC = () => {
             </div>
             <div>
                 <div className="flex flex-row w-full px-10 justify-center gap-6">
-                    <div className="bg-white w-[30%]">
-                        <h2 className="font-semibold text-[#0F3B57]">Total de Análises:</h2>
-                        <FontAwesomeIcon icon={faChartSimple} />
+                    <div className="bg-white w-[30%] rounded-lg border border-solid border-[#c0c1c9] shadow-[0_0_60px_rgba(0,0,0,0.15)]">
+                        <div className="flex flex-row justify-between p-3 items-center">
+                            <h2 className="font-semibold text-[#0F3B57] text-2xl">Total de Análises:</h2>
+                            <FontAwesomeIcon icon={faChartSimple} className="bg-[#F2F0EB] px-1 py-2 rounded-lg text-2xl text-[#0F3B57]"/>
+                        </div>
                     </div>
                     <div className="bg-white w-[30%]">
                         <h2>Total de PartNumbers:</h2>
@@ -47,17 +49,32 @@ const HistoryPage: React.FC = () => {
                         <h2>Período das Análises:</h2>
                     </div>
                 </div>
+                <div className="flex flex-row w-full px-10 justify-center gap-6">
+                    
+                </div>
             </div>
             <div>
-                {data ? (
-                    data.map((task) => (
-                        <div key={task.id} className="bg-white">
-                            <p>{task.id}</p>
+            {data && data.length > 0 ? (
+                data.map((task) => (
+                <div key={task.id} className="bg-white p-4 mb-4 rounded shadow">
+                    {/* Para cada classificação da task */}
+                    {task.classifications?.map((classification, index) => {
+                    const code = classification?.tipi?.partnumber?.code;
+                    return (
+                        <div key={index} className="mb-2">
+                        {code ? (
+                            <h1 className="text-2xl font-semibold text-[#010A26]">{code}</h1>
+                        ) : (
+                            <p className="text-gray-500 italic">Código não disponível</p>
+                        )}
                         </div>
-                    ))) : (
-                        <p>No data available.</p>
-                    )
-                }
+                    );
+                    })}
+                </div>
+                ))
+            ) : (
+                <p>No data available.</p>
+            )}
             </div>
         </div>
     );
